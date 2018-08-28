cc53b2ed79f9cef6b0c9bd3ccc993176ab720602
qge2 <qge2@ncsu.edu>
2018-08-28 09:49:31 -0400

    modify lr schedule

--- src/helper/trainer.py
+++ src/helper/trainer.py
@@ -125,10 +125,10 @@ class Trainer(object):
                              'cls_loss', 'cls_accuracy']
         cur_summary = None
         cur_epoch = unlabel_data.epochs_completed
-
-        if self.epoch_id == 50:
-            self._lr = self._lr / 10
+        self.epoch_id += 1
         if self.epoch_id == 150:
+            self._lr = self._lr / 10
+        if self.epoch_id == 200:
             self._lr = self._lr / 10
 
         step = 0
